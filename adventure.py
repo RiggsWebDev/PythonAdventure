397aee3766278a74b91706a61fc3189a1028cdc3
Riggs <52392247+RiggsWebDev@users.noreply.github.com>
2021-08-20 23:23:36 -0500

    Add files via upload

--- adventure.py
+++ adventure.py
@@ -2,7 +2,17 @@ answer = input("Would you like to play? (Yes/No)")
 
 if answer.lower().strip() == "yes":
 
-    answer = input("You reach a crossroads, would you like to go left or right? (left/right)")
+    answer = input("You reach a crossroads, would you like to go left or right? (left/right)").lower().strip()
+    if answer == "left":
+        answer = input("You see a huge dragon, as a lowly adventurer would you choose to attack or run?")
+
+        if answer == "attack":
+            print("You obviously died, you should really work on your ability to infer information...")
+        
+        if answer == "run":
+            print("Good choice! The dragon was much faster and you've now died!")
+    if answer == "right":
+        print("You walk around aimlessly in a forest eventually falling into a snake pit and dying!")
 
 else:
-    print("Goodbye")
+    print("Goodbye!")
