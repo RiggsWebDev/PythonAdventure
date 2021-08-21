ff8aa8a36246ac5dafa247655f9928343947fa0d
Riggs <52392247+RiggsWebDev@users.noreply.github.com>
2021-08-20 23:14:33 -0500

    Add files via upload

---
+++ adventure.py
@@ -0,0 +1,8 @@
+answer = input("Would you like to play? (Yes/No)")
+
+if answer.lower().strip() == "yes":
+
+    answer = input("You reach a crossroads, would you like to go left or right? (left/right)")
+
+else:
+    print("Fuck off then bitch")
